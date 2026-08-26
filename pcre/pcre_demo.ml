9ef2f969e9305fd1c157f0b454360252c9515272
Matt Teichman <teichman@uchicago.edu>
2026-08-26 16:29:18 -0500

    flesh out pcre binary

--- pcre/pcre_demo.ml
+++ pcre/pcre_demo.ml
@@ -1,2 +1,14 @@
-let () = print_endline "dude"
+let rex = Pcre2.regexp {|\d{3}-\d{4}|}
 
+let string = "phone: 555-1234"
+
+let print_output input =
+  if Pcre2.pmatch ~rex input
+  then Printf.printf
+         "the string '%s' contains a phone number\n%!"
+         input
+  else Printf.printf
+         "the string '%s' does not contain a phone number\n%!"
+         input
+
+let () = print_output string
