24ade8b298425c294df401e41eb6572e1f04e8d4
Matt Teichman <teichman@uchicago.edu>
2026-09-03 10:22:23 -0500

    printf-ify

--- readline/readline_demo.ml
+++ readline/readline_demo.ml
@@ -2,4 +2,4 @@ let () =
   Readline.init () ;
   match Readline.readline ~prompt:"Type a string: " () with
   | None -> ()
-  | Some line -> print_endline ("You said: " ^ line)
+  | Some line -> Printf.printf "You typed '%s'!\n%!" line
