e055fe99deae68e49f6b09d67fce99a2e704f387
Matt Teichman <teichman@uchicago.edu>
2026-09-03 11:00:48 -0500

    handle the empty string case

--- readline/readline_demo.ml
+++ readline/readline_demo.ml
@@ -2,4 +2,5 @@ let () =
   Readline.init () ;
   match Readline.readline ~prompt:"Type a string: " () with
   | None -> ()
+  | Some "" -> print_endline "You didn't type anything!"
   | Some line -> Printf.printf "You typed '%s'!\n%!" line
